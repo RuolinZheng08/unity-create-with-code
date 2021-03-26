7b31994d37c70449e830e811277dadc686eed6d3
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-03-25 19:21:33 -0700

    Use object pooling

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 15.0f;
-    [SerializeField] float turnSpeed = 15.0f;
+    private float speed;
+    private float rpm;
+    [SerializeField] float horsePower = 0;
+    [SerializeField] float turnSpeed = 15;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] List<WheelCollider> allWheels;
+
+    void Start() {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
 
     void FixedUpdate()
     {
+        if (!IsOnGround()) {
+            return;
+        }
         // move car forward
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f); // mph
+        speedText.text = "Speed: " + speed + " mph";
+        rpm = (speed % 30) * 40;
+        rpmText.text = "RPM: " + rpm;
+    }
+
+    bool IsOnGround() {
+        foreach (WheelCollider wheel in allWheels) {
+            if (!wheel.isGrounded) {
+                return false;
+            }
+        }
+        return true;
     }
 }
